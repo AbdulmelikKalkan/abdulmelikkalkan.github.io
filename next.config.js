90b1c7c16413872c6f8c55d72d5e928c44aa7e5c
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-01-15 16:40:46 +0300

    Update configs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  },
   async redirects() {
     return [
       {
         source: '/cv',
         destination: '/about',
-        permanent: false,
-      },
+        permanent: false
+      }
     ]
   }
 }
