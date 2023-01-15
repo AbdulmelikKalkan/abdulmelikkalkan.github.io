6a15d67c4030a21c27cfce2584bb62ba90375c95
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-01-15 17:39:56 +0300

    Update configs

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ import { NextResponse, NextRequest } from 'next/server'
 export function middleware(request) {
   console.log("hola")
   console.log(request.url)
-  return NextResponse.redirect(new URL('/about', request.url))
+  return NextResponse.redirect(new URL('/contact', request.url))
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/cv',
+  matcher: '/about',
 }
