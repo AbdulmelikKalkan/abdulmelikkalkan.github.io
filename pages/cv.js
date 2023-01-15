bdf27a26db8d38c00412d76707091b522cab1d61
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-01-15 13:25:51 +0300

    Add cv document

---
+++ pages/cv.js
@@ -0,0 +1,9 @@
+import Main from '../components/main'
+
+export default function Home() {
+  return (
+    <Main>
+      <iframe src="https://docs.google.com/document/d/e/2PACX-1vSZN6v7sX6Vk7PI5lXgpoHKJY7_4hgkUsVWQuuinaJuOTznN8bMzLMmF-cb48rTUPYNaeITyBP50sOP/pub?embedded=true"></iframe>
+    </Main>
+  )
+}
