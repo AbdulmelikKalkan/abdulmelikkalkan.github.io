266786995d7993914aae78709ec959dbea74f65b
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-01-15 14:45:43 +0300

    Add: redirecting cv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: 'https://docs.google.com/document/d/1_5kFQWujxgYVaWwZpiSBG5B7_qGexIW23jl0LLg5PjA/preview#',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/cv.js
+++
@@ -1,9 +0,0 @@
-import Main from '../components/main'
-
-export default function Home() {
-  return (
-    <Main>
-      <iframe src="https://docs.google.com/document/d/e/2PACX-1vSZN6v7sX6Vk7PI5lXgpoHKJY7_4hgkUsVWQuuinaJuOTznN8bMzLMmF-cb48rTUPYNaeITyBP50sOP/pub?embedded=true"></iframe>
-    </Main>
-  )
-}
